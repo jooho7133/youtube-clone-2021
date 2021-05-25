015825a8bdcabed3f886153563149a284c2112bc
jooho7133 <jooho7133@gmail.com>
2021-05-25 20:58:38 +0900

    #3.3 Responses

--- src/server.js
+++ src/server.js
@@ -4,9 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is tring to go home.");
+const handleHome = (req, res) => {
+  //   return res.end();
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server Listening on port  http://localhost/${PORT}`);
