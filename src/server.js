95f8e1d310e3d0990b1aa25dd9861fb2b4aa54ab
jooho7133 <jooho7133@gmail.com>
2021-05-25 20:03:02 +0900

    #3.2 GET Request part Two

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is tring to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`✅ Server Listening on port  http://localhost/${PORT}`);
 
