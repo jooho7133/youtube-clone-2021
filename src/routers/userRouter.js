fbbe374ae07236eb113b4b61623c381e6cf4e537
jooho7133 <jooho7133@gmail.com>
2021-05-28 00:58:07 +0900

    #4.8 URL Parameters part Two

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 userRouter.get("/login", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
-userRouter.get(":id", see);
+userRouter.get(":id(\\d+)", see);
 
 export default userRouter;
