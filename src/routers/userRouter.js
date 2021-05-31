183d098d454e602120e924acbf9a7b1d0bfbd493
jooho7133 <jooho7133@gmail.com>
2021-05-31 14:04:03 +0900

    #5.7 Conditionals

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,7 +3,7 @@ import { edit, logout, remove, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get("/login", logout);
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
 userRouter.get(":id(\\d+)", see);
