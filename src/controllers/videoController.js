8436efc29c120783c4a0fd9bb5b1442a7622a90d
jooho7133 <jooho7133@gmail.com>
2021-06-01 20:30:25 +0900

    #6.3 Edit Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -39,4 +39,10 @@ export const getEdit = (req, res) => {
   return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  const video = videos.find((video) => video.id == id);
+  video.title = title;
+  return res.redirect(`/videos/${id}`);
+};
