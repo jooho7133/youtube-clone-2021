bf50c9f3c8012c8288a82847c51b165dd9a2cd11
jooho7133 <jooho7133@gmail.com>
2021-06-12 14:51:42 +0900

    #7.12 MongoStore

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,9 +3,9 @@ import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, email, username, password1, password2, location } = req.body;
+  const { name, email, username, password, password2, location } = req.body;
   const pageTitle = "Join";
-  if (password1 !== password2) {
+  if (password !== password2) {
     return res.status(400).render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match.",
