558538f62425116df83ae48763b9688f695754e2
jooho7133 <jooho7133@gmail.com>
2021-06-12 16:13:36 +0900

    #7.14 Expiration and Secrets

--- src/server.js
+++ src/server.js
@@ -16,14 +16,17 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
+console.log(process.env);
+
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({
-      mongoUrl: "mongodb://127.0.0.1:27017/youtube-clone-2021",
-    }),
+    // cookie: {
+    //   maxAge: 20000,
+    // },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
