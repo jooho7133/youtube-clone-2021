e4fdccf225fc650af70401007a3893198b134952
jooho7133 <jooho7133@gmail.com>
2021-06-16 02:10:21 +0900

    #8.6 File Uploads part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -141,7 +141,9 @@ export const postEdit = async (req, res) => {
       user: { _id },
     },
     body: { name, email, username, location },
+    file,
   } = req;
+  console.log(file);
   const exists = await User.exists({
     $and: [{ _id: { $ne: _id } }, { $or: [{ username }, { email }] }],
   });
