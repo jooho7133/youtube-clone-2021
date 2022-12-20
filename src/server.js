9074ed22a9eff99cccb959997401699da17bcd26
Jooho Kim <jhkim@pineone.com>
2022-12-21 00:42:58 +0900

    #3.2 GET Request part Two

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Someone is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`✅ Server listening on http://localhost:${PORT}`);
 
