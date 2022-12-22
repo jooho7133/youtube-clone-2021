98bf93ae46eb49b4036e26ccc9047005197ac46a
Jooho Kim <jhkim@pineone.com>
2022-12-22 23:45:38 +0900

    #3.5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,14 +4,14 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
 const handleHome = (req, res) => {
-  res.end();
+  res.send("I love middleware");
 };
-const handleLogin = (req, res) => {
-  res.send("Login here.");
-};
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listening on http://localhost:${PORT}`);
